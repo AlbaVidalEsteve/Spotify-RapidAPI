20f9c52ad5770526c3baa0dd6daaaaa77b82e2bd
AlbaVidalEsteve <66486832+AlbaVidalEsteve@users.noreply.github.com>
2024-03-25 22:19:39 +0100

    clean comments

--- components/footer.php
+++
@@ -1,7 +0,0 @@
-<div class="footer">
-    <ul>
-        <li><a href="#">Cookies</a></li>
-        <li><a href="#">Política de privacidad</a></li>
-        <li><a href="#">Aviso Legal</a></li>
-    </ul>
-</div>
--- js/hamburger-script.js
+++
@@ -1,9 +0,0 @@
-const BOTON = document.querySelector('#icono i');
-const ENLACES = document.querySelector('#enlaces');
-
-BOTON.addEventListener('click', ()=>{
-    ENLACES.classList.toggle('open');
-    setTimeout(() =>{
-        BOTON.classList.toggle('fa-xmark');
-    }, 600);
-});
